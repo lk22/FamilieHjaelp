21d323665e9ee25c8122b36b4539725b20738b59
Leo Knudsen <lk22>
2025-06-29 17:53:29 +0200

    translating login and register title and description

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -32,7 +32,7 @@ export default function Register() {
     };
 
     return (
-        <AuthLayout title="Create an account" description="Enter your details below to create your account">
+        <AuthLayout title="Opret din konto" description="Udfyld formularen for at oprette en konto og begynd at bruge FamilieHjælp.">
             <Head title="Register" />
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
