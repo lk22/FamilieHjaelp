042c975200a47330236780cc9b6af78a6ec1e59d
lk22 <nightskater2200@gmail.com>
2025-08-06 21:39:49 +0200

    adding current step from onboarding state into getting started page for better navigation to the step

--- resources/js/pages/home/getting-started.tsx
+++ resources/js/pages/home/getting-started.tsx
@@ -13,6 +13,12 @@ const GettingStartedContent = ({step}: {step?: string}) => {
     console.log('GettingStartedContent rendered with auth:', auth);
     console.log('Onboarding State:', onboardingState);
 
+    const completedSteps = onboardingState.completedSteps;
+    const currentStep = completedSteps.lastIndexOf(completedSteps[completedSteps.length - 1]) + 1 || 1; // Default to step 1 if no steps are completed
+
+    // get last completed step or default to step 1
+    console.log('Current Step:', currentStep);
+
     return (
         <>
             <Head title={`Kom i gang | ${name}`} />
@@ -25,7 +31,7 @@ const GettingStartedContent = ({step}: {step?: string}) => {
                     </div>
                 </nav>
             </header>
-            <main className="bg-[#0a0a0a] text-white min-h-screen">
+            <main className="bg-[#0a0a0a] text-white h-screen">
                 <div className="container-fluid py-8 max-w-full flex w-full flex-col items-center justify-center bg-[#004EA7] text-white bg-[#0a0a0a]">
                     <div className="container max-w-[960px] flex-col py-8 items-center justify-center text-center">
                         <div className="logo">
@@ -47,52 +53,103 @@ const GettingStartedContent = ({step}: {step?: string}) => {
                         </div>
                     </div>
                 </div>
-                <div className="container-fluid bg-white max-w-full w-full">
+                <div className="container-fluid bg-white max-w-full w-full h-screen">
                     <div className="container max-w-[960px] px-4 py-8 mx-auto text-black">
-                        <p className="mt-6 text-lg font-normal">
-                            Nogle oplevelser ændrer livet fra det ene øjeblik til det andet. At miste et barn — uanset hvor langt man er i graviditeten — er en sorg, der kan være svær at sætte ord på. Det er en tid fyldt med følelser, spørgsmål og beslutninger, som man aldrig havde forestillet sig at skulle tage.
-                        </p>
-                        <p className="mt-6 text-lg font-normal">
-                            Uanset hvor du står lige nu, er du ikke alene. {name} er skabt for dig, der står midt i eller efter et tab. Her er der plads til både sorg, tvivl, vrede,
-                        </p>
-                        <p className="mt-6 text-lg font-normal">
-                            kærlighed og savn — og ingen følelser er forkerte.
-                        </p>
-                        <p className="mt-6 text-lg font-normal">
-                            FamilieHjælp giver dig nødvendig information og dine muligheder og rettigheder i din situation.
-                        </p>
-                        <p className="mt-6 text-lg font-normal">
-                            Du finder råd og støtte til at navigere i den svære tid både praktisk og følelsesmæssigt.
-                        </p>
-                        <p className="mt-6 text-lg font-normal">
-                            Du behøver ikke gå vejen alene. Her er der plads til dig, lige som du er, med det du bærer på.
-                        </p>
-                        <p className="mt-6 text-lg font-bold">
-                            Velkommen
-                        </p>
-                        <nav className="flex mt-8 items-center justify-start gap-4">
-                            {auth.user && auth.user.has_completed_onboarding ? (
-                                <Link
-                                    href={route('dashboard')}
-                                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-white hover:border-white dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                                >
-                                    Dashboard
-                                </Link>
-                            ) : (
-                                <>
-                                    <Link 
-                                        href={route('onboarding.step', {_query: {step: step}})} 
-                                        className="inline-block rounded-sm border border-[#004EA7] bg-[#004EA7] px-5 py-1.5 text-xl leading-normal text-white hover:border-white"
-                                    >
-                                        Start
-                                    </Link>
-                                </>
-                            )}
-                        </nav>
+                        {
+                            currentStep > 0 ? 
+                                <CompletedStepContent step={currentStep} /> : 
+                                (
+                                    <>
+                                        <p className="mt-6 text-lg font-normal">
+                                            Nogle oplevelser ændrer livet fra det ene øjeblik til det andet. At miste et barn — uanset hvor langt man er i graviditeten — er en sorg, der kan være svær at sætte ord på. Det er en tid fyldt med følelser, spørgsmål og beslutninger, som man aldrig havde forestillet sig at skulle tage.
+                                        </p>
+                                        <p className="mt-6 text-lg font-normal">
+                                            Uanset hvor du står lige nu, er du ikke alene. {name} er skabt for dig, der står midt i eller efter et tab. Her er der plads til både sorg, tvivl, vrede,
+                                        </p>
+                                        <p className="mt-6 text-lg font-normal">
+                                            kærlighed og savn — og ingen følelser er forkerte.
+                                        </p>
+                                        <p className="mt-6 text-lg font-normal">
+                                            FamilieHjælp giver dig nødvendig information og dine muligheder og rettigheder i din situation.
+                                        </p>
+                                        <p className="mt-6 text-lg font-normal">
+                                            Du finder råd og støtte til at navigere i den svære tid både praktisk og følelsesmæssigt.
+                                        </p>
+                                        <p className="mt-6 text-lg font-normal">
+                                            Du behøver ikke gå vejen alene. Her er der plads til dig, lige som du er, med det du bærer på.
+                                        </p>
+                                        <p className="mt-6 text-lg font-bold">
+                                            Velkommen
+                                        </p>
+                                        <nav className="flex mt-8 items-center justify-start gap-4">
+                                            {auth.user && auth.user.has_completed_onboarding ? (
+                                                <Link
+                                                    href={route('dashboard')}
+                                                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-white hover:border-white dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                                >
+                                                    Dashboard
+                                                </Link>
+                                            ) : (
+                                                <>
+                                                    <Link 
+                                                        href={route('onboarding.step', {_query: {step: step}})} 
+                                                        className="inline-block rounded-sm border border-[#004EA7] bg-[#004EA7] px-5 py-1.5 text-xl leading-normal text-white hover:border-white"
+                                                    >
+                                                        Start
+                                                    </Link>
+                                                </>
+                                            )}
+                                        </nav>
+                                    
+                                    </>
+                                )
+                        }
                     </div>
                 </div>
             </main>
         </>
+    );
+}
+
+const CompletedStepContent = ({ step }: { step: number }) => {
+    const { onboardingState } = useOnboarding();
+    const { auth } = usePage<SharedData>().props;
+
+    const currentStepData = onboardingState.steps.find(s => s.id === step);
+    console.log('Current Step Data:', currentStepData);
+
+    return (
+        <div className="container mx-auto py-8">
+            <h2 className="text-3xl font-bold mb-4">Du er kommet til trin {step}</h2>
+            {
+                currentStepData?.id === 6 ? (
+                    <>
+                        <p className="text-lg mb-4">
+                            Du har gennemført alle trin i onboarding-processen. Du kan nu begynde at udforske de forskellige ressourcer og muligheder, der er tilgængelige for dig.
+                        </p>
+                        <Link
+                            href={`${
+                                auth?.user?.id ? route('dashboard') : route('onboarding.complete')
+                            }`}
+                            className="inline-block mr-4 bg-blue-900 text-white px-4 py-2 rounded-sm hover:bg-blue-800 transition-colors duration-300"
+                        >
+                            Færdiggør onboarding
+                        </Link>
+                    </>
+                ) : (
+                    <>    
+                        <p className="text-lg mb-4">
+                            Du har gennemført trin {step} i onboarding-processen. Du kan nu fortsætte til næste trin.
+                        </p>
+                        <Link
+                            href={route('onboarding.step', { step: currentStepData?.name || 1 })}
+                            className="inline-block bg-blue-900 text-white px-4 py-2 rounded-sm hover:bg-blue-800 transition-colors duration-300"
+                        >
+                            Fortsæt til næste trin
+                        </Link>
+                    </>
+            )}
+        </div>
     );
 }
 
