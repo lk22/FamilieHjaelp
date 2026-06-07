4e170a6d3922ef44082c0dd54c0bcf15534e538b
lk22 <nightskater-220@hotmail.com>
2026-06-07 23:41:39 +0200

    (WIP) - mobile navigation

--- resources/js/components/WebLayout/MainNav.tsx
+++ resources/js/components/WebLayout/MainNav.tsx
@@ -1,14 +1,15 @@
 import { useState, useEffect, useCallback } from "react";
 
 import MemoizedMainNavigation from "./Navigation/MainNavigation";
-import MobileNavigation from "./Navigation/MobileNavigation";
+import MemoizedMobileNavigation from "./Navigation/MobileNavigation";
+import {Icon} from "@/components/icon";
 
 export default function MainNav() {
   const [isAuthDialogOpen, setIsAuthDialogOpen] = useState<boolean>(false);
   const [isMobileNav, setIsMobileNav] = useState<boolean>(false);
 
   const handleResize = useCallback(() => {
-    if (window.innerWidth < 768) {
+    if (window.innerWidth < 1024) {
       setIsMobileNav(true);
     } else {
       setIsMobileNav(false);
@@ -41,14 +42,24 @@ export default function MainNav() {
     setIsAuthDialogOpen(false);
   }, []);
 
+  const toggleMobileNav = useCallback(() => {
+    setIsMobileNav((prev) => !prev);
+  }, []);
+
   return (
     <>
       {isMobileNav ? (
-        <MobileNavigation
-          openAuthDialog={openAuthDialog}
-          closeAuthDialog={closeAuthDialog}
-          isAuthDialogOpen={isAuthDialogOpen}
-        />
+        <>
+          <button onClick={toggleMobileNav} className="text-white lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+          </button>
+          <MemoizedMobileNavigation
+            openAuthDialog={openAuthDialog}
+            closeAuthDialog={closeAuthDialog}
+            isAuthDialogOpen={isAuthDialogOpen}
+            isNavOpen={isMobileNav}
+          />
+        </>
       ) : (
         <MemoizedMainNavigation
           openAuthDialog={openAuthDialog}
--- resources/js/components/WebLayout/Navigation/MobileNavigation.tsx
+++ resources/js/components/WebLayout/Navigation/MobileNavigation.tsx
@@ -9,12 +9,13 @@ interface MainNavigationProps {
   openAuthDialog: () => void;
   closeAuthDialog: () => void;
   isAuthDialogOpen: boolean;
+  isNavOpen?: boolean;
 }
 
-const MobileNavigation = ({ openAuthDialog, closeAuthDialog, isAuthDialogOpen }: MainNavigationProps) => {
+const MobileNavigation = ({ openAuthDialog, closeAuthDialog, isAuthDialogOpen, isNavOpen }: MainNavigationProps) => {
   return (
-    <nav>
-      <ul className="flex justify-center gap-4">
+    <nav className={`w-full bg-blue-900 h-full fixed bottom-0 top-0 -left-1000 transition-transform ${isNavOpen ? 'translate-x-0' : '-translate-x-full'}`} id="mobile-nav">
+      <ul className="flex flex-col items-center justify-center gap-4">
         <li>
           <Link href={route('page.help-resources')} className="text-white hover:text-white">Hjælpemidler</Link>
         </li>
@@ -23,7 +24,7 @@ const MobileNavigation = ({ openAuthDialog, closeAuthDialog, isAuthDialogOpen }:
         </li>
         <li>
           <Link href={route('page.experiences.abortion')} className="text-white hover:text-white">Har du oplevet</Link>
-          <ul>
+          <ul className="flex gap-2">
             <li><Link href={route('page.experiences.abortion')} className="text-white hover:text-white">Abort</Link></li>
             <li><Link href={route('page.experiences.stillbirth')} className="text-white hover:text-white">Dødfødsel</Link></li>
             <li><Link href={route('page.experiences.new-parents')} className="text-white hover:text-white">Nybagte forældre</Link></li>
