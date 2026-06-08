8e2b77d9f55473733c6555c65dcdbac17b3564d1
lk22 <nightskater-220@hotmail.com>
2026-06-08 20:50:21 +0200

    fix namespace bug for HTTPS Scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,8 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Support\Facades\URL;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         Model::shouldBeStrict(! app()->isProduction());
         // allow HTTPS on all environments, since the app is behind a reverse proxy that handles SSL termination
         if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
+            \Illuminate\Support\Facades\URL::forceScheme('https');
         }
 
         if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
