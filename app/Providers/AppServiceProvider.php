3d2b04e6da3dfee0daf1361a938b4f10fb64b946
lk22 <nightskater-220@hotmail.com>
2026-06-08 20:46:44 +0200

    fix namespace bug for HTTPS Scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Database\Eloquent\Model;
-use Iluminate\Support\Facades\URL;
 
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
