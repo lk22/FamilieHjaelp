8f91740d3823beb1211b075a533c07c172baf7b6
lk22 <nightskater-220@hotmail.com>
2026-06-22 01:49:12 +0200

    working on mobile navigation menu

--- resources/js/components/WebLayout/MainNav.tsx
+++ resources/js/components/WebLayout/MainNav.tsx
@@ -4,10 +4,14 @@ import MemoizedMainNavigation from "./Navigation/MainNavigation";
 import MemoizedMobileNavigation from "./Navigation/MobileNavigation";
 
 export default function MainNav() {
-  const [isAuthDialogOpen, setIsAuthDialogOpen] = useState<boolean>(false);
-  const [isMobileNav, setIsMobileNav] = useState<boolean>(false);
+  const [ isAuthDialogOpen, setIsAuthDialogOpen ] = useState<boolean>(false);
+  const [ isMobileNav, setIsMobileNav ] = useState<boolean>(false);
+  const [ isNavOpen, setIsNavOpen ] = useState<boolean>(false);
+
+  console.log(isAuthDialogOpen);
 
   const handleResize = useCallback(() => {
+    console.log('Window resized:', window.innerWidth);
     if (window.innerWidth < 1024) {
       setIsMobileNav(true);
     } else {
@@ -18,10 +22,16 @@ export default function MainNav() {
   const handleClickOutside = useCallback((event: MouseEvent) => {
     const target = event.target;
 
-    if (target instanceof HTMLElement && !target.closest('dialog')) {
-      setIsAuthDialogOpen(false);
+    // only apply this logic if the auth dialog is open
+
+    // only apply this logic on desktop views
+    if (window.innerWidth >= 1024) {
+      if (target instanceof HTMLElement && !target.closest('dialog')) {
+        setIsAuthDialogOpen(false);
+      }
     }
-  }, []);
+
+  }, [isAuthDialogOpen]);
 
   useEffect(() => {
     handleResize();
@@ -42,21 +52,20 @@ export default function MainNav() {
   }, []);
 
   const toggleMobileNav = useCallback(() => {
-    setIsMobileNav((prev) => !prev);
+    setIsNavOpen((prev) => !prev);
   }, []);
+
 
   return (
     <>
       {isMobileNav ? (
         <>
-          <button onClick={toggleMobileNav} className="text-white lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
-          </button>
           <MemoizedMobileNavigation
             openAuthDialog={openAuthDialog}
             closeAuthDialog={closeAuthDialog}
             isAuthDialogOpen={isAuthDialogOpen}
-            isNavOpen={isMobileNav}
+            isNavOpen={isNavOpen}
+            toggleMobileNav={toggleMobileNav}
           />
         </>
       ) : (
---
+++ resources/js/components/WebLayout/Navigation/MobileNavigation/MobileNavigationFooter.tsx
@@ -0,0 +1,17 @@
+import LanguageSwitcher from "../LanguageSwitcher";
+
+export default function MobileNavigationFooter() {
+  return (
+    <div className="mobile-nav-footer absolute bottom-0 left-0 w-full p-4 border-t border-gray-700 text-center">
+      <div className="mobile-nav-lang-swither mb-4">
+        <LanguageSwitcher />
+      </div>
+      <div className="flex mb-8">
+        <div className="email">
+          <a href="mailto:info@familiehjaelp.dk" className="text-white hover:text-gray-400 text-lg">E-mail: <span className="font-bold">info@familiehjaelp.dk</span></a>
+        </div>
+      </div>
+      <p className="text-sm text-gray-400">&copy; {new Date().getFullYear()} FamilieHjaelp. All rights reserved.</p>
+    </div>
+  )
+}
