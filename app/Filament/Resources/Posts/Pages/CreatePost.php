69d714e8b30749d412af8f5cadd31b0912edfdad
lk22 <nightskater-220@hotmail.com>
2026-09-04 11:49:34 +0200

    disabling default exception handling but sending af notification to the user

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -3,9 +3,13 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
 
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+use Filament\Support\Exceptions\Halt;
+use Throwable;
 
 class CreatePost extends CreateRecord
 {
@@ -18,5 +22,21 @@ class CreatePost extends CreateRecord
         $locale = $data['locale'] ?? 'da';
         $data['url'] = url($locale . '/blog/articles/' . $data['slug']);
         return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        try {
+            return parent::handleRecordCreation($data);
+        } catch (Throwable $exception) {
+            report($exception);
+            Notification::make()
+                ->title('Failed to create the post.')
+                ->body($exception->getMessage())
+                ->danger()
+                ->send();
+
+            throw new Halt();
+        }
     }
 }
