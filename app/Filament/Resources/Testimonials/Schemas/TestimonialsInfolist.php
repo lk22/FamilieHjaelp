fd34172bd99dc1bda5d99198b91091063d9628a3
lk22 <nightskater-220@hotmail.com>
2026-09-21 18:45:40 +0200

    adding entry fields for testimonial records in backend

--- app/Filament/Resources/Testimonials/Schemas/TestimonialsInfolist.php
+++ app/Filament/Resources/Testimonials/Schemas/TestimonialsInfolist.php
@@ -3,6 +3,10 @@
 namespace App\Filament\Resources\Testimonials\Schemas;
 
 use Filament\Schemas\Schema;
+use Illuminate\Support\HtmlString;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
 
 class TestimonialsInfolist
 {
@@ -10,7 +14,16 @@ class TestimonialsInfolist
     {
         return $schema
             ->components([
-                //
+                TextEntry::make('title'),
+                TextEntry::make('name'),
+                TextEntry::make('locale'),
+                ImageEntry::make('image'),
+                TextEntry::make('content')
+                    ->columnSpanFull()
+                    ->formatStateUsing(fn ($state) => new HtmlString(
+                        \Illuminate\Support\Str::limit($state, 100)
+                    ))
+                    ->limit(50),
             ]);
     }
 }
